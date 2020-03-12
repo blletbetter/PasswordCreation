2b02977fc5e87f4bfd9ba2fc228caa205956174f
Ben Letbetter <ben.letbetter@outlook.com>
2020-03-12 10:38:27 -0400

    Clean up

--- AccountManager.cs
+++ AccountManager.cs
@@ -7,9 +7,10 @@ namespace PasswordCreation
 {
     public class AccountManager
     {
-        Dictionary<string, object> accounts = new Dictionary<string, object>();
-        private  string Password { get; set; }
-        public  void NewAccount(string username, object password)
+        Dictionary<string, string> accounts = new Dictionary<string, string>();
+        
+        
+        public  void NewAccount(string username, string password)
         {
             try
             {
@@ -19,37 +20,45 @@ namespace PasswordCreation
             {
                 Console.WriteLine("The username you entered already exist. try again.");
                 Console.ReadKey();
-                Run();
+                CreateAccount();
             }
         }
 
-        public  void Run()
+        public  void CreateAccount()
         {
             using SHA256 sha256 = SHA256.Create();
-            string username, password;
+            string username, password1, password2;
 
             Console.WriteLine("Please enter a username: ");
             username = Console.ReadLine();
             Console.Clear();
-            password = Password;
+            password2 = "";
+            password1 = password2;
             do
             {
-                if (password != Password)
+                if (password1 != password2)
                 {
                     Console.Clear();
                     Console.WriteLine("Passwords did not match. Please try again.");
                 }
                 Console.WriteLine("Please create a password for your account: ");
-                password = Console.ReadLine();
+                password1 = Console.ReadLine();
                 Console.WriteLine("Please confirm the password for your account: ");
-                Password = Console.ReadLine();
-            } while (password != Password);
-
-            
-            NewAccount(username, Password);
+                password2 = Console.ReadLine();
+            } while (password1 != password2);
+            password2 = EncryptPW(password1);
+            NewAccount(username, password2);
             Console.Clear();
+            Console.WriteLine(password2);
             Console.WriteLine("New Account created");
 
+        }
+
+        public string EncryptPW(string password)
+        {
+            byte[] byts = Encoding.ASCII.GetBytes(password);
+            string gibberish = Convert.ToBase64String(byts);
+            return gibberish;
         }
 
         public void AccessAccount()
@@ -60,6 +69,7 @@ namespace PasswordCreation
             username = Console.ReadLine();
             Console.WriteLine("Password: ");
             password = Console.ReadLine();
+            password = EncryptPW(password);
             DisplayAccount(username, password);
         }
 
--- Program.cs
+++ Program.cs
@@ -7,44 +7,43 @@ namespace PasswordCreation
     {
         static void Main(string[] args)
         {
-            //int input;
-            //AccountManager account = new AccountManager();
-            //do
-            //{
-            //    Console.Clear();
-            //    MainMenu();
-            //    var val = Console.ReadLine();
-            //    while (ValidOption(val, out input) == false)
-            //    {
-            //        MainMenu();
-            //        val = Console.ReadLine();
-            //    }
-            //    switch (input)
-            //    {
-            //        case 1:
-            //            Console.Clear();
-            //            account.Run();
-            //            Console.WriteLine("Press any key to continue...");
-            //            Console.Read();
-            //            break;
-            //        case 2:
-            //            Console.Clear();
-            //            account.AccessAccount();
-            //            Console.WriteLine("Press any key to continue...");
-            //            Console.ReadKey();
-            //            break;
-            //        case 3:
-            //            Console.Clear();
-            //            account.DisplayAllAccounts();
-            //            return;
-            //        default:
-            //            Console.WriteLine("\n\nInvalid option, please try again.");
-            //            Console.Clear();
-            //            break;
+            int input;
+            AccountManager account = new AccountManager();
+            do
+            {
+                Console.Clear();
+                MainMenu();
+                var val = Console.ReadLine();
+                while (ValidOption(val, out input) == false)
+                {
+                    MainMenu();
+                    val = Console.ReadLine();
+                }
+                switch (input)
+                {
+                    case 1:
+                        Console.Clear();
+                        account.CreateAccount();
+                        Console.WriteLine("Press any key to continue...");
+                        Console.Read();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        account.AccessAccount();
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        account.DisplayAllAccounts();
+                        return;
+                    default:
+                        Console.WriteLine("\n\nInvalid option, please try again.");
+                        Console.Clear();
+                        break;
 
-            //    }
-            //} while (input != 3);
-            
+                }
+            } while (input != 3);
 
         }
 
@@ -52,6 +51,7 @@ namespace PasswordCreation
         {
             Console.WriteLine("Welcome to Account Creation Manager! What would you like to do?");
             Console.WriteLine("Options:\n1.) Create New Account\n2.) Login to account\n3.) Exit application");
+            Console.WriteLine("Your choice: ");
         }
         static bool ValidOption(string input, out int option)
         {
